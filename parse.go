b0c492ce5ca7704258c7b937a78abed70c02dc36
egeerdogan <ege@erdogan.dev>
2020-06-07 11:06:08 +0300

    checks for date

--- parse.go
+++ parse.go
@@ -3,26 +3,57 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/smtp"
 	"os"
+	"time"
 
 	"github.com/mmcdole/gofeed"
 )
+
+const days = 7
 
 func main() {
 	file, err := os.Open("feeds.txt")
 	check(err)
 	defer file.Close()
 
+	dateThreshold := time.Now().AddDate(0, 0, -days)
+
 	fp := gofeed.NewParser()
+	msg := "RSS FEEDS \n"
+
+	start := time.Now()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		feed, _ := fp.ParseURL(scanner.Text())
-		fmt.Println(feed.Title)
+		msg += "\n" + feed.Title + "\n"
 		for i := 0; i < len(feed.Items); i++ {
-			fmt.Printf("\t%s - %s\n", feed.Items[i].Published, feed.Items[i].Title)
+			if feed.Items[i].PublishedParsed.After(dateThreshold) {
+				msg += "\t" + feed.Items[i].Published + "  -  " + feed.Items[i].Title + "\n"
+			}
 		}
 	}
+
+	fmt.Println(time.Since(start).Seconds())
+	// send("ege@erdogan.dev", msg)
+}
+
+func send(to, body string) {
+	from := os.Getenv("GMAIL_NAME")
+	pass := os.Getenv("GMAIL_PASS")
+
+	msg := "From: " + from + "\n" +
+		"To: " + to + "\n" +
+		"Subject: RSS Feeds\n\n" +
+		body
+
+	err := smtp.SendMail("smtp.gmail.com:587",
+		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+		from, []string{to}, []byte(msg))
+	check(err)
+
+	fmt.Printf("Sent mail to %s\n", to)
 }
 
 func check(e error) {
