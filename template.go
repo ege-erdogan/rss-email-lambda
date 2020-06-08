b0c0415ab03153a41211cabe355e1081c1fe3d3d
egeerdogan <ege@erdogan.dev>
2020-06-08 16:36:33 +0300

    template path

--- template.go
+++ template.go
@@ -6,18 +6,20 @@ import (
 	"io/ioutil"
 )
 
+const templatesPath = "template/"
+
 // GenerateMessage creates main message
 func GenerateMessage(blocks []string) string {
 	var htmlBlocks []template.HTML
 	for _, text := range blocks {
 		htmlBlocks = append(htmlBlocks, template.HTML(text))
 	}
-	return executeTemplate("template/main.html", htmlBlocks)
+	return executeTemplate(templatesPath+"main.html", htmlBlocks)
 }
 
 // GenerateHTMLFeedBlock create HTML template for one feed
 func GenerateHTMLFeedBlock(feed Feed) string {
-	return executeTemplate("template/feed.html", feed)
+	return executeTemplate(templatesPath+"feed.html", feed)
 }
 
 func executeTemplate(filePath string, data interface{}) string {
