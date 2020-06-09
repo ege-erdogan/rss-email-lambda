554f23c4f79a74aea2fad8fba3f864c12f99081f
egeerdogan <ege@erdogan.dev>
2020-06-09 11:43:29 +0300

    template

--- template.go
+++ template.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"html/template"
 	"io/ioutil"
+	"time"
 )
 
 const templatesPath = "template/"
@@ -11,10 +12,17 @@ const templatesPath = "template/"
 // GenerateMessage creates main message
 func GenerateMessage(blocks []string) string {
 	var htmlBlocks []template.HTML
+	htmlBlocks = append(htmlBlocks, template.HTML(GenerateHeader()))
 	for _, text := range blocks {
 		htmlBlocks = append(htmlBlocks, template.HTML(text))
 	}
 	return executeTemplate(templatesPath+"main.html", htmlBlocks)
+}
+
+// GenerateHeader creates header HTML for feed
+func GenerateHeader() string {
+	date := time.Now().Format("January 2, 2006")
+	return executeTemplate(templatesPath+"header.html", date)
 }
 
 // GenerateHTMLFeedBlock create HTML template for one feed
@@ -31,4 +39,9 @@ func executeTemplate(filePath string, data interface{}) string {
 	err = tmpl.Execute(&doc, data)
 	check(err)
 	return doc.String()
+}
+
+// GetCurrentDate returns current date as formatted string
+func GetCurrentDate() string {
+	return time.Now().Format("January 2, 2020")
 }
